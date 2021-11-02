be3ae714fac7f5f272a2292f65ec655b5660cd73
Matt Fellenz <mattf53190@gmail.com>
2021-11-02 15:27:04 -0700

    Initialize app before using it

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const server = http.createServer(app);
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(__dirname + "/public"));
 
